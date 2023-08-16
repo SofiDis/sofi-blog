1efd1bf145502a49d17bd6e16c219d5172e9c5f4
Sofidis <sofia.dissegna93@gmail.com>
2023-08-16 13:17:09 +0100

    Fix temp type

--- src/middlewares/dbConnection.ts
+++ src/middlewares/dbConnection.ts
@@ -1,6 +1,6 @@
 import { connectToDatabase } from "../core/mongoDb";
 
-export const dbConnection = async function (_req, _res, next) {
+export const dbConnection = async function (_req: any, _res: any, next: any) {
   console.log("Try connection to db");
   await connectToDatabase();
   next();
